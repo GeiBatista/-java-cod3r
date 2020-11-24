50d4690a44f1468a0b8112cb63df249748f3c4dd
GeiBatista <geibatista@hotmail.com>
2020-11-23 21:40:57 -0300

    Desafio Array

---
+++ exercicios/src/arrays/DesafioArray.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DesafioArray {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+				
+		System.out.print("Quantas notas você deseja informar? ");
+		
+		int qtdNotas = entrada.nextInt();
+		
+		double notas[] = new double[qtdNotas];
+		
+				
+		for (int i = 0; i < notas.length; i++) {
+			System.out.printf("Digite a nota %d%s", i + 1, ": " );
+			notas[i] = entrada.nextDouble();
+		}
+		
+		double total = 0;
+		for(double nota : notas) {
+		total += nota; 
+		}
+		
+		System.out.printf("A média é :%.2f", (total / notas.length));
+		entrada.close();
+	}
+
+}
