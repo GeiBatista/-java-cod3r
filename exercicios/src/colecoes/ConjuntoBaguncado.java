2c23fd168039698a21882d061275d2a7f9f6c2bd
GeiBatista <geibatista@hotmail.com>
2020-12-08 22:35:17 -0300

    conjunto set

--- exercicios/src/colecoes/ConjuntoBaguncado.java
+++ exercicios/src/colecoes/ConjuntoBaguncado.java
@@ -1,6 +1,7 @@
 package colecoes;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class ConjuntoBaguncado {
 
@@ -15,7 +16,36 @@ public class ConjuntoBaguncado {
 		conjunto.add(1);
 		conjunto.add('x');
 		
-		System.out.println("Tamanho � " + conjunto.size());
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		conjunto.add("Teste");
+		conjunto.add('x');
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		System.out.println(conjunto.remove("teste"));
+		System.out.println(conjunto.remove("Teste"));
+		System.out.println(conjunto.remove('x'));
+		
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		System.out.println(conjunto.contains('x'));
+		System.out.println(conjunto.contains(1));
+		System.out.println(conjunto.contains(true));
+		
+		Set nums = new HashSet();
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		//conjunto.addAll(nums); // União entre dois conjuntos
+		conjunto.retainAll(nums);
+		System.out.println(conjunto);
+		
+		conjunto.clear();
+		System.out.println(conjunto);
 	}
 
 }
